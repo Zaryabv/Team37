af835de625d8ec0eb0250249f868201c7db87352
JoelDucram <116264636+JoeBoss12@users.noreply.github.com>
2022-11-03 10:20:22 +0000

    Update admin.py

--- Cleats/shop/store/admin.py
+++ Cleats/shop/store/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from store.models import Basket,Item,BasketItem
 
-# Register your models here.
+admin.site.register(Basket)
+admin.site.register(Item)
+admin.site.register(BasketItem)
